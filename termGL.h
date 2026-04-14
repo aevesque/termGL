cacd8f5563e5008801a82f9e58642af9970cdbaa
aevesque <141275616+aevesque@users.noreply.github.com>
2026-04-14 20:44:58 +0200

    added vt100 graphic mode mappings as defines

--- termGL.h
+++ termGL.h
@@ -33,4 +33,28 @@ void	destroyFrame(Frame *frame);
 void	displayFrame(Frame *frame);
 void	clearFrame(Frame *frame);
 
+/* VT100 graphic mappings */
+# define PIXEL		'a'
+# define DOT		'~'
+# define DIAMOND	'`'
+
+# define ULEFT_CORNER	'l'
+# define DLEFT_CORNER	'm'
+# define URIGHT_CORNER	'i'
+# define LRIGHT_CORNER	'j'
+
+# define HLINE_1	's'
+# define HLINE_2	'r'
+# define HLINE_3	'q'
+# define HLINE_4	'p'
+# define HLINE_5	'o'
+
+# define VLINE		'x'
+
+# define CROSSING_LINE	'n'
+
+# define LEFT_T		't'
+# define RIGHT_T	'u'
+# define BOTTOM_T	'v'
+# define TOP_T		'w'
 #endif
